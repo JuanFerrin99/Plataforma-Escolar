43a0048c0fd5bc6b8e6f2a45d9540a87b052ca85
santiagopatinio <mitens2004@gmail.com>
2022-10-03 17:13:07 -0300

    Reset password

--- FrontEnd/src/pages/LoginPage.jsx
+++ FrontEnd/src/pages/LoginPage.jsx
@@ -3,7 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -13,29 +12,22 @@ import axios from "axios";
 import { useState } from "react";
 import Cookies from "js-cookie";
 
-function Copyright(props) {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
-            <Link color="inherit" href="https://mui.com/">
-                Your Website
-            </Link>{' '}
-            {new Date().getFullYear()}
-            {'.'}
-        </Typography>
-    );
-}
 
 const theme = createTheme();
 
 export default function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    
+    const [pressed, setPressed] = useState(false);
+
+    const handleEnviarReset = (event) => {
+        event.preventDefault();
+        handleResetPassword()
+        window.location.href = '/login'
+    }
     const handleEmailChange = (event) => {
         setEmail(event.target.value)
     }
-
     const handlePasswordChange = (event) => {
         setPassword(event.target.value)
     }
@@ -45,77 +37,144 @@ export default function SignIn() {
     };
 
     const handleLogin = () => {
-
         const body = {
             email: email,
             password: password
         }
- 
+
         const options = {
             withCredentials: true
-        }  
-        
+        }
+
         axios.post("http://localhost:3001/login", body, options)
             .then(jwt => {
                 window.location.href = `/${Cookies.get("rol")}`
             })
-            .catch(e => console.log(e+"daw"));
+            .catch(e => console.log(e + "daw"));
+    }
+    const handleResetPassword = () => {
+        const options = {
+            withCredentials: true
+        }
+
+        axios.post("http://localhost:3001/login/password/reset", { email: email }, options)
+            .then(jwt => {
+            })
+            .catch(e => console.log(e + "daw"));
     }
 
+    function cambiarContraseña(props) {
+        return (
+            <Typography variant="body2" color="text.secondary" align="center" {...props}>
+                <button onClick={() => {
+                    setPressed(true)
+                }}>
+                    Restabler contraseña
+                </button>
+            </Typography>
+        );
+    }
 
-    return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            onChange={handleEmailChange}
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            onChange={handlePasswordChange}
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign In
-                        </Button>
+    if (pressed === false) {
+        return (
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                onChange={handleEmailChange}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                onChange={handlePasswordChange}
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                        </Box>
                     </Box>
-                </Box>
-                <Copyright sx={{ mt: 8, mb: 4 }} />
-            </Container>
-        </ThemeProvider>
-    );
+                    {cambiarContraseña()}
+                </Container>
+            </ThemeProvider>
+        );
+    }
+    else {
+        return (
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Ingrese su mail
+                        </Typography>
+                        <Box component="form" onSubmit={handleEnviarReset} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                onChange={handleEmailChange}
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Enviar
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </ThemeProvider>
+        )
+    }
 }
