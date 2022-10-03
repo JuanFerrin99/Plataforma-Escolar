ac009cc76fcfd237de74ec2a780431c222108cb9
brunocala7 <bcalabrese04@gmail.com>
2022-10-03 17:17:53 -0300

    Css homepage

--- FrontEnd/src/components/Carrusel.jsx
+++ FrontEnd/src/components/Carrusel.jsx
@@ -6,12 +6,15 @@ export default function Example(props)
 {
     var items = [
         {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
+            name: "Solicita ahora tu beca",
+            description: "Beca 50% Ciclo Lectivo 2023. 10 Cuotas de Marzo a Diciembre"
         },
         {
-            name: "Random Name #2",
-            description: "Hello World!"
+            name: "Tus Primeros Pasos en Electrónica",
+            description: "Unite a los Cursos Cortos de Electrónica Básica del ISE"
+        },{
+            name: "Conocé el instituto",
+            description: "Desde 1962, el Instituto Superior de Electrónica formó más de 5.000 profesionales electrónicos."
         }
     ]
 
--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -1,15 +1,20 @@
-//import '../styles/paginas/HomePage.css';
+import '../styles/HomePage.css';
 import { Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Carrusel from "../components/Carrusel"
 export default function  Hub() {
     return (
         <body id="a">
-            <p>hay que guardar el mail en algunn lgar que no se cambiable por el usuario y hay que proteger ciertas urls de ciertos usuarios</p>
-            <img src="https://images.unsplash.com/photo-1626593261859-4fe4865d8cb1?ixlib=rb-1.2.1ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8MTYlM0E5fGVufDB8fDB8fA%3D%3Dw=1000q=80" alt="" width="100%" height="50%" id='imagen'/>
+            <div id="infoInicio">
+
+            <p>contactanos</p>
+
+            </div>  
+
             <div id= "carrucel_div"><Carrusel /></div>
             <div id="contacto">
-                <p>contactanos</p>
+
+            <p>contactanos</p>
 
             </div>  
             <Container>
