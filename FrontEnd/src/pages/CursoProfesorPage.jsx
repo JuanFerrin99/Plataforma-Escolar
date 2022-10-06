ef5001bc050514058096cd5b465dcf531a4528ec
Ferrin <ferrin@elmejor.com>
2022-10-06 17:38:14 -0300

    errorrws en sistencias

--- FrontEnd/src/pages/CursoProfesorPage.jsx
+++ FrontEnd/src/pages/CursoProfesorPage.jsx
@@ -39,9 +39,13 @@ function Variants() {
 
 export default function CursoCard({ }) {
     const [alumnos, setAlumnos] = useState([]);
+    const [fechasAsistencia, setFechasAsistencia] = useState([]);
+    const [diasCursados, setDiasCursados] = useState([]);
     const [loading, setLoading] = useState(true);
     const [isPressed, setIsPressed] = useState(false);
+    const [isPressedAsistencia, setIsPressedAsistencia] = useState(false);
     const [rows, setRows] = useState([]);
+    const [date, setDate] = useState("");
     const location = useLocation()
     const id = location.state.idCurso // id del curso que se esta mostrando
 
@@ -50,7 +54,10 @@ export default function CursoCard({ }) {
             .then(response => response.json())
             .then(curso => {
                 setAlumnos(curso.alumnos)
+                setFechasAsistencia(curso.fechasAsistencia)
+                setDiasCursados(curso.diasCursados)
                 setRows([])
+                setDate(new Date)
                 curso.alumnos.forEach((element) => {
                     setRows((oldState) => [...oldState, { "id": element.dni, "Apellido": element.apellido, "Nombre": element.nombre }])
                 })
@@ -60,16 +67,23 @@ export default function CursoCard({ }) {
             .catch(error => {
                 console.log(error)
             })
-        }, []);
-        
-        const alumnosComponent = alumnos.map((alumno, i) => {
-            return <AlumnoCard key={alumno._id} id={id} nombre={alumno.nombre} apellido={alumno.apellido} dni={alumno.dni} />
-        })
-        
-        const alumnosSkeleton = new Array(20).fill(<Variants />)
-        if (isPressed === false) {
-            return (
-                <div>
+    }, []);
+
+    const diaCorrecto = () => {
+        return diasCursados.includes(new Date.getDay())
+    }
+
+    const asitenciaNoTomada = () => {
+        return fechasAsistencia.includes(`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`)
+    }
+    const alumnosComponent = alumnos.map((alumno, i) => {
+        return <AlumnoCard key={alumno._id} id={id} nombre={alumno.nombre} apellido={alumno.apellido} dni={alumno.dni} />
+    })
+
+    const alumnosSkeleton = new Array(20).fill(<Variants />)
+    if (isPressed === false) {
+        return (
+            <div>
                 MATERIA
                 <Button id="botonAsistencia" variant="contained" onClick={() => { setIsPressed(true) }} endIcon={<AddIcon />}>
                     Tomar asistencia
@@ -82,25 +96,49 @@ export default function CursoCard({ }) {
                 </Grid>
                 <Container>
                     <Outlet />
-                </Container>    
+                </Container>
             </div>
         );
     }
-    else {     
-        console.log(rows)
+    else {
+        const onRowsSelectionHandler = (ids) => {
+            if (isPressedAsistencia == true) {
+                //if (diaCorrecto() && asitenciaNoTomada()) {
+                    ids.map((id) => rows.find((row) => row.id === id)).forEach((alumno)=>{
+                        useEffect(() => {
+                            fetch("http://localhost:3001/inasistencias/", {
+                                method: 'POST',
+                                headers: {
+                                  'Accept': 'application/json, text/plain, */*',
+                                  'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({})
+                              }).then(res => res.json())
+                                .then(res => console.log(res));
+                        }, []);
+                    })
+                    setIsPressedAsistencia(true)
+                //}
+                //else (console.log("no se puede tomar asistencia"))//! mostrar en pantalla
+            }
+            else return
+        };
+
         return (
             <div style={{ height: 400, width: '100%' }}>
                 <DataGrid
                     rows={rows}
                     columns={columns}
-                    pageSize={5}
+                    pageSize={50}
                     rowsPerPageOptions={[5]}
                     checkboxSelection
-                    />
+                    onSelectionModelChange={(ids) => onRowsSelectionHandler(ids)}
+                />
+                <button onClick={() => { setIsPressedAsistencia(true) }}>Tomar asistencia</button>
             </div>
         );
     }
-    
+
 }
 
 
