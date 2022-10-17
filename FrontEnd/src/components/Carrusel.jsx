8ab5053a2991d92ca418104d877de519d5ddd4d0
brunocala7 <bcalabrese04@gmail.com>
2022-10-17 17:40:47 -0300

    css y carousel

--- FrontEnd/src/components/Carrusel.jsx
+++ FrontEnd/src/components/Carrusel.jsx
@@ -1,42 +1,46 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel'
-import { Paper, Button } from '@mui/material'
-
-export default function Example(props)
-{
-    var items = [
-        {
-            name: "Solicita ahora tu beca",
-            description: "Beca 50% Ciclo Lectivo 2023. 10 Cuotas de Marzo a Diciembre"
-        },
-        {
-            name: "Tus Primeros Pasos en Electrónica",
-            description: "Unite a los Cursos Cortos de Electrónica Básica del ISE"
-        },{
-            name: "Conocé el instituto",
-            description: "Desde 1962, el Instituto Superior de Electrónica formó más de 5.000 profesionales electrónicos."
-        }
-    ]
-
-    return (
-        <Carousel>
-            {
-                items.map( (item, i) => <Item key={i} item={item} /> )
+import Carousel from "react-material-ui-carousel";
+import { makeStyles } from "@material-ui/core";
+import '../styles/HomePage.css';
+const items = [
+    {
+        name: "Solicita ahora tu beca",
+        description: "Beca 50% Ciclo Lectivo 2023. 10 Cuotas de Marzo a Diciembre"
+    },
+    {
+        name: "Tus Primeros Pasos en Electrónica",
+        description: "Unite a los Cursos Cortos de Electrónica Básica del ISE"
+    },{
+        name: "Conocé el instituto",
+        description: "Desde 1962, el Instituto Superior de Electrónica formó más de 5.000 profesionales electrónicos."
+    }
+];
+const useStyles = makeStyles((theme) => ({}));
+export default function App() {
+  return (
+    <div className="Carousel">
+      <div className="slider">
+        <Carousel
+          indicatorIconButtonProps={{
+            style: {
+              padding: "10px", // 1
+              color: "#1c2538"
             }
+          }}
+          indicatorContainerProps={{
+            style: {
+              marginTop: "30px"
+            }
+          }}
+        >
+          {items.map((element, index) => (
+            <>
+            <h1 key={index}>{element.name}</h1>
+            <p key={index}>{element.description}</p>
+            <button id="botonInfo">+ INFO</button>
+            </>
+          ))}
         </Carousel>
-    )
-}
-
-function Item(props)
-{
-    return (
-        <Paper>
-            <h2>{props.item.name}</h2>
-            <p>{props.item.description}</p>
-
-            <Button className="CheckButton">
-                + INFO
-            </Button>
-        </Paper>
-    )
+      </div>
+    </div>
+  );
 }
--- FrontEnd/src/pages/HomePage.jsx
+++ FrontEnd/src/pages/HomePage.jsx
@@ -2,24 +2,30 @@ import '../styles/HomePage.css';
 import { Container } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import Carrusel from "../components/Carrusel"
-export default function  Hub() {
+export default function Hub() {
     return (
         <body id="a">
 
             <div id="infoInicio">
-            <h1>Instituto Manuel Savio</h1>
-            <p>Carrera de Técnico Superior en electrónica</p>
-            <p>Título Oficial de validez Nacional</p>
-            <button>CARRERA</button>
-            <button>CURSOS</button>
-            </div>  
+                <h1>Instituto Manuel Savio</h1>
+                <p>Carrera de Técnico Superior en electrónica</p>
+                <p>Título Oficial de validez Nacional</p>
+                <button>CARRERA</button>
+                <button>CURSOS</button>
+            </div>
 
-            <div id= "carrucel_div"><Carrusel /></div>
+            <div id="carrucel_div"><Carrusel /></div>
             <div id="contacto">
-
-            <p>contactanos</p>
-
-            </div>  
+                <div id="info">
+                    <h1>Contactate con<br />
+                    nosotros</h1>
+                    <p> mail<br/>
+                    info@ise.com.ar<br/>
+                    telefono<br/>
+                    +54 9 11 3611-7780</p>
+                </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13132.78512711098!2d-58.4503281!3d-34.6244798!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7b474047508c8258!2sInstituto%20Superior%20de%20Electr%C3%B3nica%20Manuel%20Savio!5e0!3m2!1ses-419!2sar!4v1666024268209!5m2!1ses-419!2sar&zoom=21”" allowfullscreen=""loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
             <Container>
                 <Outlet />
             </Container>
