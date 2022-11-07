05b603b7ee5cdea0ba6a02cae4358d6c88e030fe
brunocala7 <bcalabrese04@gmail.com>
2022-11-07 12:50:03 -0300

    hola

--- FrontEnd/src/pages/Header.jsx
+++ FrontEnd/src/pages/Header.jsx
@@ -104,6 +104,4 @@ export default function ResponsiveAppBar () {
 };
 
 
-
-
     
--- FrontEnd/src/pages/InfoInstitucional.jsx
+++ FrontEnd/src/pages/InfoInstitucional.jsx
@@ -1,14 +1,31 @@
 import React from "react";
-
-import Hero from "../components/infoInstitucional/hero.jsx";
-import Post from "../components/infoInstitucional/postContainer";
 import '../styles/pages/InfoInstitucional.css';
 
 export default function Info() {
   return (
-    <div className="Info">
-      <Hero />
-      <Post />
+    <div  className="Info">
+      <img id="imgHero" src="https://ise.com.ar/wp-content/uploads/2021/08/cropped-GettyImages-1226985345-scaled-1.jpg" />
+      <div id="encabezado">
+        <h1>El Instituto</h1>
+        <h2>Formando Profesionales desde 1962</h2>
+      </div>
+      <p id="textoInfo">
+        En el Instituto Superior de Electrónica Manuel Savio (ISE) A-558, afrontamos exitosamente la responsabilidad de crear, modificar y actualizar los cursos de electrónica.<br />
+
+        Nos incorporamos a la enseñanza oficial como instituto de electrónica, y contamos con más de 60 años otorgando títulos oficiales. Partiendo de un curso de electrónica básica para principiantes.<br />
+
+        Nuestra modalidad de cursada, de Lunes a Viernes con horarios fijos nos compara con una escuela de electrónica, haciendo el curso de técnico en electrónica de forma completa o por materias. Terminando la carrera de técnico superior en electrónica en 3 años y medio.<br />
+
+        Es nuestro objetivo inspirar al técnico en electrónica la avidez por perfeccionarse alcanzando los mejores niveles de conocimiento en su carrera electrónica para que puedan participar activamente en los cambios de la sociedad a la que pertenecen e inculcar en ellos la necesidad de ser quienes logren reincorporar los valores éticos, a la tecnología y el proceso.<br />
+
+        Considerando el modelo de sociedad actual, nos parece necesario brindar cursos de electrónica más completos y actualizados, para que al egresar puedan insertarse en los puestos de trabajo disponibles compitiendo lealmente con sus colegas, tomando decisiones en los niveles más altos de la empresa o ámbito donde se desarrollan sus actividades para contribuir al crecimiento socioeconómico. Teniendo la posibilidad de continuar en ingeniería en electrónica en otra institución.<br />
+
+        Dada la relevancia adquirida por las telecomunicaciones, la robótica y la programación es cada vez mayor la demanda de técnicos capaces de trabajar en estas especialidades a fin de perfeccionarlas y permitir su desarrollo, el cual resulta indispensable para un país en crecimiento y en camino hacia una economía abierta al mercado tecnológico.<br />
+
+        Por todo esto hemos incorporado el taller de robótica, electromedicina y electrónica industrial. Hemos actualizado las clases sobre microcontroladores, radiofrecuencia, sistemas de control y PLC.<br />
+        Hemos reforzado el estudio de dispositivos y circuitos electrónicos. Hemos incrementado las prácticas de laboratorio en mas de 240hs, donde ofrecemos todos los materiales y el instrumental necesarios.<br />
+        Ofrecemos talleres para la fabricación de las herramientas para el futuro profesional.
+      </p>
     </div>
   );
 }
